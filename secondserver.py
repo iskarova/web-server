db0d00b6a3c3fd2410e0b754c70fa09c8cf7bcec
lina iskarova <cyberdeadushka@gmail.com>
2025-05-03 01:10:23 +0300

    display a listing of a directory's contents

--- secondserver.py
+++ secondserver.py
@@ -1,44 +1,80 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import os
 
+
 class ServerException(Exception):
-  pass
+    pass
+
+class case_no_file(object):
+    def test(self, handler):
+      return not os.path.exists(handler.full_path)
+  
+    def act(self, handler):
+      raise ServerException("'{0}' not found".format(handler.path))
+  
+  
+class case_existing_file(object):
+    def test(self, handler):
+      return os.path.isfile(handler.full_path)
+  
+    def act(self, handler):
+      handler.handle_file(handler.full_path)
+    
+    
+class case_always_fail(object):
+    def test(self, handler):
+      return True
+  
+    def act(self, handler):
+      raise ServerException("Unknown object '{0}'".format(handler.path))    
+    
+    
+class case_dir_index_file(object):
+    def index_path(self, handler):
+      return os.path.join(handler.full_path, 'index.html')
+  
+    def test(self, handler):
+      return os.path.isdir(handler.full_path) and \
+            os.path.isfile(self.index_path(handler))
+    
+    def act(self, handler):
+      handler.handle_file(self.index_path(handler))
 
 
+class case_dir_no_index_file(object):
+    def index_path(self, handler):
+      return os.path.join(handler.full_path, 'index.html')
+  
+    def test(self, handler):
+      return os.path.isdir(handler.full_path) and \
+            not os.path.isfile(self.index_path(handler))
+    
+    def act(self, handler):
+      handler.list_dir(handler.full_path)
+    
+    
+    
 class RequestHandler(BaseHTTPRequestHandler):
-  "'Handle HTTP requests by returning a fixed page.'"
-  # Page to send back
-  Page = '''\
-      <html>
-        <body>
-          <table>
-            <tr>  <td>Header</td>        <td>Value</td>         </tr>
-            <tr>  <td>Date and time</td> <td>{date_time}</td>   </tr> 
-            <tr>  <td>Client host</td>   <td>{client_host}</td> </tr>
-            <tr>  <td>Client port</td>   <td>{client_port}</td> </tr> 
-            <tr>  <td>Command</td>       <td>{command}</td>     </tr>
-            <tr>  <td>Path</td>          <td>{path}</td>        </tr> 
-          </table>
-        </body>
-      </html>
-    '''
 
+  Cases = [case_no_file(),
+           case_existing_file(),
+           case_dir_index_file(),
+           case_dir_no_index_file(),
+           case_always_fail()]
 
 #Handle a GET request
   def do_GET(self):
     try:    
       # What exactly is being requested?
-      full_path = os.getcwd() + self.path
-      print("Current directory: ", full_path)
+      self.full_path = os.getcwd() + self.path
+      print("FULL PATH: ", self.full_path)
       
-      if not os.path.exists(full_path):
-        raise ServerException("'{0}' not found".format(self.path))
-      
-      elif os.path.isfile(full_path):
-        self.handle_file(full_path)
-        
-      else:
-        raise ServerException("Unknown object '{0}'".format(self.path))
+      for case in self.Cases:
+        handler = case
+        if handler.test(self):
+          print("CASE: ", case)
+          handler.act(self)
+          break
       
     except Exception as msg:
       self.handle_error(msg)
@@ -69,23 +105,43 @@ class RequestHandler(BaseHTTPRequestHandler):
   def handle_file(self, full_path):
     try:
       with open(full_path, 'rb') as reader:
-        content = reader.read()
+        content = reader.read().decode()
       self.send_content(content)
     except IOError as msg:
       msg = "'{0}' cannot be read: {1}".format(self.path, msg)
       self.handle_error(msg)
     
     
-  def create_page(self):
-    values = {
-      'date_time'   : self.date_time_string(),
-      'client_host' : self.client_address[0],
-      'client_port' : self.client_address[1],
-      'command'     : self.command,
-      'path'        : self.path 
-    }
-    page = self.Page.format(**values)
-    return page
+  # def create_page(self):
+  #   values = {
+  #     'date_time'   : self.date_time_string(),
+  #     'client_host' : self.client_address[0],
+  #     'client_port' : self.client_address[1],
+  #     'command'     : self.command,
+  #     'path'        : self.path 
+  #   }
+  #   page = self.Page.format(**values)
+  #   return page
+  
+  
+  Listing_Page = '''\
+    <html>
+      <body>
+        <ul>{0}</ul>
+      </body>
+    </html>
+  '''
+  
+  def list_dir(self, full_path):
+    try:
+      entries = os.listdir(full_path)
+      bullets = ['<li>{0}</li>'.format(e)
+                 for e in entries if not e.startswith('.')]
+      page = self.Listing_Page.format('\n'.join(bullets))
+      self.send_content(page)
+    except OSError as msg:
+      msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
+      self.handle_error(msg)
     
 #_______________________________________________
 
---
+++ webserver.py
@@ -0,0 +1,45 @@
+import socket
+
+# Define socket host and port
+HOST = '127.0.0.1'
+PORT = 8000
+
+# Create socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server_socket.bind((HOST, PORT))
+server_socket.listen(1)
+print('Listening on port %s ...' % PORT)
+
+while True:    
+    # Wait for client connections
+    client_connection, client_address = server_socket.accept()
+
+    # Get the client request
+    request = client_connection.recv(1024).decode()
+    print(request)
+
+    # Parse HTTP headers
+    headers = request.split('\n')
+    filename = headers[0].split()[1]    
+
+    # Get the content of the file
+    if filename == '/':
+        filename = '/index2.html'
+
+    try:
+        fin = open('docs' + filename)
+        content = fin.read()
+        fin.close()
+
+        response = 'HTTP/1.0 200 OK\n\n' + content
+    except FileNotFoundError:
+        response = 'HTTP/1.0 404 NOT FOUND\n\nFile not found'
+
+    # Send HTTP response
+    
+    client_connection.sendall(response.encode())
+    client_connection.close()
+
+# Close socket
+server_socket.close()
