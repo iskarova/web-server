041dc1ee35e608d120a9168ca11f6e305bd46376
lina iskarova <cyberdeadushka@gmail.com>
2025-05-02 21:00:27 +0300

    rename first server file

---
+++ secondserver.py
@@ -0,0 +1,48 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class RequestHandler(BaseHTTPRequestHandler):
+  "'Handle HTTP requests by returning a fixed page.'"
+  # Page to send back
+  Page = '''\
+      <html>
+        <body>
+          <table>
+            <tr>  <td>Header</td>        <td>Value</td>         </tr>
+            <tr>  <td>Date and time</td> <td>{date_time}</td>   </tr> 
+            <tr>  <td>Client host</td>   <td>{client_host}</td> </tr>
+            <tr>  <td>Client port</td>   <td>{client_port}</td> </tr> 
+            <tr>  <td>Command</td>       <td>{command}</td>     </tr>
+            <tr>  <td>Path</td>          <td>{path}</td>        </tr> 
+          </table>
+        </body>
+      </html>
+    '''
+
+#Handle a GET request
+  def do_GET(self):
+    page = self.create_page()
+    self.send_page(page)
+
+  def create_page(self):
+    values = {
+      'date_time'   : self.date_time_string(),
+      'client_host' : self.client_address[0],
+      'client_port' : self.client_address[1],
+      'command'     : self.command,
+      'path'        : self.path 
+    }
+    page = self.Page.format(**values)
+    return page
+    
+  def send_page(self, page):
+    self.send_response(200)
+    self.send_header("Content-Type", "text/html")
+    self.send_header("Contenet-Length", str(len(self.Page)))
+    self.end_headers()
+    self.wfile.write(page.encode('utf-8'))
+
+
+if __name__ == '__main__':
+  server_address = ('127.0.0.1', 8080)
+  server = HTTPServer(server_address, RequestHandler)
+  server.serve_forever()
